8390665328b7b41537f8a388e96e4fa5bdfeb696
changLifan <13820636756@163.com>
2020-09-16 13:09:13 +0800

    third

--- remind me.java
+++ remind me.java
@@ -21,3 +21,17 @@ git remote add origin https://github.com/changLifan/Forward_Secrecy.git
 git branch -M master
 git push -u origin master
 
+1. 下载安装Git客户端
+https://blog.csdn.net/ezreal_tao/article/details/81609883
+
+Windows下TortoiseGit客户端安装到Git分支使用详细教程（非必要）
+https://blog.csdn.net/hello_world_qwp/article/details/80857558
+
+
+2. 配置用户名密码
+git config --global user.name  "changLifan"  
+git config --global user.email  "13820636756@163.com"
+
+user.name=changLifan
+user.email=13820636756@163.com
+
